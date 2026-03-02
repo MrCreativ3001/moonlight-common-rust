e017fef8c0fc28fe579d7179462f3c319ac5837d
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2026-03-02 20:35:39 +0100

    removed openssl backend from tokio impl

--- examples/client-tokio.rs
+++ examples/client-tokio.rs
@@ -3,7 +3,7 @@
 use std::sync::Arc;
 
 use moonlight_common::{
-    crypto::openssl::OpenSSLCryptoBackend,
+    crypto::{openssl::OpenSSLCryptoBackend, rustcrypto::RustCryptoBackend},
     high::tokio::MoonlightHost,
     http::{
         DEFAULT_HTTP_PORT, DEFAULT_UNIQUE_ID,
@@ -33,7 +33,7 @@ async fn main() {
             .unwrap();
 
     // Create a Crypto Backend
-    let crypto_provider = Arc::new(OpenSSLCryptoBackend::default());
+    let crypto_provider = Arc::new(RustCryptoBackend::default());
 
     // Try to get existing identity
     match try_load_identity_async().await {
