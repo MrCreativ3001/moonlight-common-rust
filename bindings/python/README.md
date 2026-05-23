b5141626c1e245941141ee209a3afd14ca36e8e7
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2026-05-23 19:20:49 +0200

    generate bindings to python using uniffi

---
+++ bindings/python/README.md
@@ -0,0 +1,5 @@
+
+# `moonlight-common-rust` Bindings for Python
+
+This is an example of how to generate bindings using [UniFFI](https://github.com/mozilla/uniffi-rs) to `moonlight-common-rust` for python.
+It is mostly used for testing.
---
+++ bindings/python/build.py
@@ -0,0 +1,52 @@
+import os
+import platform
+import subprocess
+import shutil
+from pathlib import Path
+
+# Fallback to local target dir if env var is not set
+CARGO_TARGET_DIR = os.environ.get("CARGO_TARGET_DIR", "target")
+
+FEATURES = "uniffi"
+
+# Determine correct dynamic library extension for current OS
+SYSTEM = platform.system()
+LIB_NAME = "moonlight_common"
+
+if SYSTEM == "Windows":
+    LIB_EXT = "dll"
+    LIB_FILE = f"{LIB_NAME}.{LIB_EXT}"
+elif SYSTEM == "Darwin":
+    raise "no mac for you"
+else:
+    LIB_EXT = "so"
+    LIB_FILE = Path(CARGO_TARGET_DIR) / "debug" / f"lib{LIB_NAME}.{LIB_EXT}"
+
+LIB_OUTPUT_PATH = Path(CARGO_TARGET_DIR) / "debug" / LIB_FILE
+
+try:
+    # Build the Rust project
+    subprocess.run(["cargo", "build", "--features", FEATURES], check=True)
+
+    # Copy the output library
+    shutil.copy2(LIB_OUTPUT_PATH, f"./{LIB_FILE}")
+
+    # Generate UniFFI Python bindings
+    subprocess.run(
+        [
+            "cargo",
+            "run",
+            "-p",
+            "uniffi-bindgen",
+            "generate",
+            str(LIB_OUTPUT_PATH),
+            "--language",
+            "python",
+            "--out-dir",
+            "./",
+        ],
+        check=True,
+    )
+except subprocess.CalledProcessError as e:
+    print(f"\nCommand failed with exit code {e.returncode}")
+    sys.exit(e.returncode)
