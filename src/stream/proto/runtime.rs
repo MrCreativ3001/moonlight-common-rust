3afdbd54dddcee2f393313403340da315d185bba
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2026-06-28 14:32:14 +0200

    cargo fix

--- src/stream/proto/runtime.rs
+++ src/stream/proto/runtime.rs
@@ -1,22 +1,6 @@
-use std::{net::SocketAddr, pin::pin, time::Duration};
+use std::net::SocketAddr;
 
-use futures::{FutureExt, future::Fuse, select, try_join};
 use sans_io_time::Instant;
-use thiserror::Error;
-
-use crate::stream::{
-    HostFeatures, MoonlightStreamConfig, MoonlightStreamSettings,
-    audio::OpusMultistreamConfig,
-    proto::{
-        DynCryptoBackend, MoonlightStreamInput, MoonlightStreamProtoError, MoonlightStreamSetup,
-        MoonlightStreamSetupOutput,
-        audio::{AudioStream, AudioStreamError},
-        control::{ControlStream, ControlStreamEvent, peer::ControlError},
-        microphone::foundation::{FoundationMicStream, FoundationMicStreamError},
-        video::{VideoStream, VideoStreamError},
-    },
-    video::{VideoCapabilities, VideoSetup},
-};
 
 pub trait UdpStream: Send + Sync {
     type Error;
